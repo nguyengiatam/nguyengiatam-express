eeace91b3aa466556594bc9af5fee3068b382ff1
nguyengiatam <nguyengiatam.95@gmail.com>
2022-03-19 10:37:08 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path: `${__dirname}/../.env`});
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs/promises');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let dataCity = '';
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send(dataCity);
+})
+
+app.listen(PORT, async () => {
+    try {
+        dataCity = JSON.parse(await fs.readFile(`${__dirname}/../data/city.list.json`, { encoding: 'utf8' }));
+        console.log('Server ready');
+    } catch (error) {
+        console.log(error);
+    }
+})
